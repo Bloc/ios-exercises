4de63eb1fd332ed543e6a4ec32f3e707dfc8cfc1
Aaron Brager <getagb@gmail.com>
2014-06-10 16:09:51 -0500

    made equality tests fail again another attempt at fixing project issue

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -10,20 +10,12 @@
 
 @implementation EqualityDeterminer
 
-//- (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
-//    return (string1 == string2);
-//}
-//
-//- (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-//    return (number1 == number2);
-//}
-
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
-    return [string1 isEqualToString:string2];
+    return (string1 == string2);
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-    return [number1 isEqualToNumber:number2];
+    return (number1 == number2);
 }
 
 @end
