e2ed88fe1a663fb8a4a3b7220f925fd0346aa5d0
Aaron Brager <getaaron@gmail.com>
2014-06-11 12:10:53 -0500

    removed dictionary solutions

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -10,40 +10,16 @@
 
 @implementation StarTrekDictionaries
 
-//- (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-//    return @"";
-//}
-//
-//- (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
-//    return @[];
-//}
-//
-//- (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-//    return @{};
-//}
-
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    return characterDictionary[@"favorite drink"];
+    return @"";
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
-    NSMutableArray *array = [NSMutableArray array];
-    
-    for (NSDictionary *characterDictionary in charactersArray) {
-        [array addObject:characterDictionary[@"favorite drink"]];
-    }
-    
-    return array;
-}
-
-- (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
-    return [charactersArray valueForKey:@"favorite drink"];
+    return @[];
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    NSMutableDictionary *mutableDictionary = [characterDictionary mutableCopy];
-    mutableDictionary[@"quote"] = @"asdf";
-    return mutableDictionary;
+    return @{};
 }
 
 @end
