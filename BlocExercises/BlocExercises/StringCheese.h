fe35cde759ff09d389d900a95df875bac78a8255
Aaron Brager <getaaron@gmail.com>
2014-06-11 22:45:36 -0500

    Add some documentation

--- BlocExercises/BlocExercises/StringCheese.h
+++ BlocExercises/BlocExercises/StringCheese.h
@@ -10,8 +10,38 @@
 
 @interface StringCheese : NSObject
 
+/**
+ Creates a sentence indicating a favorite cheese.
+ 
+ Example usage:
+ 
+ @code
+ NSString *fullSentence = [cheese favoriteCheeseStringWithCheese:@"cheddar"];
+ // fullSentence is "My favorite cheese is cheddar."
+ @endcode
+ 
+ @param cheeseName
+ The name of the favorite cheese
+ 
+ @return Returns a full sentence indicating a favorite cheese.
+ */
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName;
 
+/**
+ Removes "cheese" from the end of a string about cheese.
+ 
+ Example usage:
+ 
+ @code
+ NSString *shortName = [cheese cheeseNameWithoutCheeseSuffix:@"mozarella cheese"];
+ // fullSentence is "My favorite cheese is cheddar."
+ @endcode
+ 
+ @param cheeseName
+ The name of the favorite cheese
+ 
+ @return Returns a full sentence indicating a favorite cheese.
+ */
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName;
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount;
